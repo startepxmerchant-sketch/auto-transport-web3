259d61682909f6bda6f8c0b41a70de5ca3ebc622
startepxmerchant-sketch <startepxmerchant@gmail.com>
2025-12-28 17:27:05 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Auto Transport Web3 Platform</h1>
+      <p>Under Construction</p>
+    </div>
+  )
+}
+
+export default App
